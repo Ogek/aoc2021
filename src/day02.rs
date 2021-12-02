0124763d0e807ba535c0881a9d70c0c8a3206098
Giuseppe Chiarella <giuseppe_chiarella@hotmail.it>
2021-12-02 09:23:25 +0100

    Refactoring

--- src/day02.rs
+++ src/day02.rs
@@ -18,14 +18,49 @@ fn parse<'a>(input: &'a str) -> impl Iterator<Item = Move> + 'a {
     input.lines().map(|m| Move::from_str(m).unwrap())
 }
 
+fn make_move(submarine: Submarine, m: Move) -> Submarine {
+    match m {
+        Move::Forward(n) => Submarine {
+            x: submarine.x + n,
+            ..submarine
+        },
+        Move::Up(n) => Submarine {
+            y: submarine.y.checked_sub(n).unwrap_or(0),
+            ..submarine
+        },
+        Move::Down(n) => Submarine {
+            y: submarine.y + n,
+            ..submarine
+        },
+    }
+}
+
 pub fn p1(input: &str) -> usize {
-    let submarine = parse(input).fold(Submarine::default(), |submarine, m| submarine.make_move(m));
+    let submarine = parse(input).fold(Submarine::default(), make_move);
 
     submarine.x * submarine.y
 }
 
+fn make_move2(submarine: Submarine, m: Move) -> Submarine {
+    match m {
+        Move::Forward(n) => Submarine {
+            x: submarine.x + n,
+            y: submarine.y + n * submarine.aim,
+            ..submarine
+        },
+        Move::Up(n) => Submarine {
+            aim: submarine.aim.checked_sub(n).unwrap_or(0),
+            ..submarine
+        },
+        Move::Down(n) => Submarine {
+            aim: submarine.aim + n,
+            ..submarine
+        },
+    }
+}
+
 pub fn p2(input: &str) -> usize {
-    let submarine = parse(input).fold(Submarine::default(), |submarine, m| submarine.make_move2(m));
+    let submarine = parse(input).fold(Submarine::default(), make_move2);
 
     submarine.x * submarine.y
 }
@@ -39,43 +74,6 @@ impl FromStr for Move {
             Some(("up", n)) => Ok(Move::Up(n.parse().unwrap())),
             Some(("down", n)) => Ok(Move::Down(n.parse().unwrap())),
             _ => Err(()),
-        }
-    }
-}
-
-impl Submarine {
-    fn make_move(self, m: Move) -> Submarine {
-        match m {
-            Move::Forward(n) => Submarine {
-                x: self.x + n,
-                ..self
-            },
-            Move::Up(n) => Submarine {
-                y: self.y.checked_sub(n).unwrap_or(0),
-                ..self
-            },
-            Move::Down(n) => Submarine {
-                y: self.y + n,
-                ..self
-            },
-        }
-    }
-
-    fn make_move2(self, m: Move) -> Submarine {
-        match m {
-            Move::Forward(n) => Submarine {
-                x: self.x + n,
-                y: self.y + n * self.aim,
-                ..self
-            },
-            Move::Up(n) => Submarine {
-                aim: self.aim.checked_sub(n).unwrap_or(0),
-                ..self
-            },
-            Move::Down(n) => Submarine {
-                aim: self.aim + n,
-                ..self
-            },
         }
     }
 }
