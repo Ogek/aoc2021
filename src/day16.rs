17dca34927ac36b7008c882538754d2c68a45205
Giuseppe Chiarella <giuseppe_chiarella@hotmail.it>
2021-12-17 06:34:40 +0100

    Day 16

--- src/day16.rs
+++ src/day16.rs
@@ -1,26 +1,25 @@
-use std::convert::TryFrom;
-use std::str::from_utf8;
+type Bits<'a> = dyn Iterator<Item = char> + 'a;
 
 #[derive(Debug, Eq, PartialEq)]
 enum Payload {
-    Literal(u32),
+    Literal(u64),
     Operator(Vec<Packet>),
 }
 
 #[derive(Debug, Eq, PartialEq)]
 struct Packet {
     version: u32,
+    typ: u8,
     payload: Payload,
 }
 
 fn parse(input: &str) -> Packet {
-    let binary = input
+    let mut bits = input
         .chars()
         .map(|c| c.to_digit(16).expect("Cannot parse hex digit"))
-        .map(|hex| format!("{:04b}", hex))
-        .collect::<String>();
+        .flat_map(|hex| format!("{:04b}", hex).chars().collect::<Vec<char>>());
 
-    Packet::try_from(binary.as_str()).expect("Cannot parse main packet")
+    Packet::parse(&mut bits)
 }
 
 fn sum_versions(packet: &Packet) -> u32 {
@@ -32,61 +31,61 @@ fn sum_versions(packet: &Packet) -> u32 {
     }
 }
 
+fn eval(packet: &Packet) -> u64 {
+    match packet.payload {
+        Payload::Literal(n) => n,
+        Payload::Operator(ref sub_packets) => match packet.typ {
+            0 => sub_packets.iter().map(eval).sum(),
+            1 => sub_packets.iter().map(eval).product(),
+            2 => sub_packets.iter().map(eval).min().unwrap(),
+            3 => sub_packets.iter().map(eval).max().unwrap(),
+            5 => (eval(&sub_packets[0]) > eval(&sub_packets[1])) as u64,
+            6 => (eval(&sub_packets[0]) < eval(&sub_packets[1])) as u64,
+            7 => (eval(&sub_packets[0]) == eval(&sub_packets[1])) as u64,
+            _ => unreachable!(),
+        },
+    }
+}
+
 pub fn p1(input: &str) -> u32 {
     sum_versions(&parse(input))
 }
 
-pub fn p2(input: &str) -> usize {
-    0
+pub fn p2(input: &str) -> u64 {
+    eval(&parse(input))
 }
 
-fn parse_literal(binary: &str) -> Payload {
-    let mut bin_idx = String::new();
-    let mut last: bool = false;
+fn parse_literal(bits: &mut Bits) -> Payload {
+    let mut literal_bits = Vec::new();
 
-    'out: for chunk in binary.as_bytes().chunks(5) {
-        if chunk[0] != b'1' {
-            last = true;
-        }
-        bin_idx.push_str(from_utf8(&chunk[1..]).unwrap());
+    loop {
+        let last = bits.next().unwrap() == '0';
+        literal_bits.extend(bits.take(4));
         if last {
-            break 'out;
+            return Payload::Literal(
+                u64::from_str_radix(&literal_bits.iter().collect::<String>(), 2).unwrap(),
+            );
         }
     }
-
-    Payload::Literal(u32::from_str_radix(bin_idx.as_str(), 2).unwrap())
 }
 
-fn parse_operator(binary: &str) -> Payload {
-    let sub_packets = match binary.chars().next().unwrap() {
+fn parse_operator(bits: &mut Bits) -> Payload {
+    let sub_packets = match bits.next().unwrap() {
         '0' => {
-            let length = usize::from_str_radix(&binary[1..16], 2).unwrap();
-            let binary = &binary[16..];
-
-            let mut got = 0;
+            let length = usize::from_str_radix(&bits.take(15).collect::<String>(), 2).unwrap();
             let mut sub_packets = Vec::new();
 
-            while got != length {
-                if length.checked_sub(got + 22).is_none() {
-                    sub_packets.push(Packet::try_from(&binary[got..length]).unwrap());
-                    got = length;
-                } else {
-                    sub_packets.push(Packet::try_from(&binary[got..got + 11]).unwrap());
-                    got += 11;
-                }
+            let mut subpacket_bits = bits.take(length).peekable();
+            while let Some(_) = subpacket_bits.peek() {
+                sub_packets.push(Packet::parse(&mut subpacket_bits));
             }
 
             sub_packets
         }
         '1' => {
-            let n_sub_packets = u32::from_str_radix(&binary[1..12], 2).unwrap();
+            let n_sub_packets = u32::from_str_radix(&bits.take(11).collect::<String>(), 2).unwrap();
 
-            binary[12..]
-                .as_bytes()
-                .chunks_exact(11)
-                .take(n_sub_packets as usize)
-                .map(|chunk| Packet::try_from(from_utf8(chunk).unwrap()).unwrap())
-                .collect()
+            (0..n_sub_packets).map(|_| Packet::parse(bits)).collect()
         }
         _ => unreachable!(),
     };
@@ -94,22 +93,23 @@ fn parse_operator(binary: &str) -> Payload {
     Payload::Operator(sub_packets)
 }
 
-impl<'a> TryFrom<&'a str> for Packet {
-    type Error = &'a str;
-
-    fn try_from(binary: &str) -> Result<Self, Self::Error> {
-        //println!("Parsing {:?}", binary);
-        let version = u32::from_str_radix(&binary[0..3], 2).unwrap();
-        let typ = u32::from_str_radix(&binary[3..6], 2).unwrap();
+impl Packet {
+    fn parse(bits: &mut Bits) -> Self {
+        let version = u32::from_str_radix(&bits.take(3).collect::<String>(), 2).unwrap();
+        let typ = u8::from_str_radix(&bits.take(3).collect::<String>(), 2).unwrap();
 
         let payload = match typ {
-            4 => parse_literal(&binary[6..]),
-            _ => parse_operator(&binary[6..]),
+            4 => parse_literal(bits),
+            _ => parse_operator(bits),
         };
 
-        let packet = Packet { version, payload };
+        let packet = Packet {
+            version,
+            typ,
+            payload,
+        };
 
-        Ok(packet)
+        packet
     }
 }
 
@@ -119,6 +119,7 @@ fn test_literal_parsing() {
         parse("D2FE28"),
         Packet {
             version: 6,
+            typ: 4,
             payload: Payload::Literal(2021)
         }
     )
@@ -130,13 +131,16 @@ fn test_operator_0_parsing() {
         parse("38006F45291200"),
         Packet {
             version: 1,
+            typ: 6,
             payload: Payload::Operator(vec![
                 Packet {
                     version: 6,
+                    typ: 4,
                     payload: Payload::Literal(10)
                 },
                 Packet {
                     version: 2,
+                    typ: 4,
                     payload: Payload::Literal(20)
                 }
             ])
@@ -150,17 +154,21 @@ fn test_operator_1_parsing() {
         parse("EE00D40C823060"),
         Packet {
             version: 7,
+            typ: 3,
             payload: Payload::Operator(vec![
                 Packet {
                     version: 2,
+                    typ: 4,
                     payload: Payload::Literal(1)
                 },
                 Packet {
                     version: 4,
+                    typ: 4,
                     payload: Payload::Literal(2)
                 },
                 Packet {
                     version: 1,
+                    typ: 4,
                     payload: Payload::Literal(3)
                 },
             ])
@@ -169,14 +177,21 @@ fn test_operator_1_parsing() {
 }
 
 #[test]
-fn test_p1() {
+fn test_p1_1() {
     assert_eq!(p1("8A004A801A8002F478"), 16);
-    // assert_eq!(p1("620080001611562C8802118E34"), 12);
-    // assert_eq!(p1("C0015000016115A2E0802F182340"), 23);
-    // assert_eq!(p1("A0016C880162017C3686B18A3D4780"), 31);
+    assert_eq!(p1("620080001611562C8802118E34"), 12);
+    assert_eq!(p1("C0015000016115A2E0802F182340"), 23);
+    assert_eq!(p1("A0016C880162017C3686B18A3D4780"), 31);
 }
 
-// #[test]
-// fn test_p2() {
-//     assert_eq!(p2("D2FE28"), 315);
-// }
+#[test]
+fn test_p2() {
+    assert_eq!(p2("C200B40A82"), 3);
+    assert_eq!(p2("04005AC33890"), 54);
+    assert_eq!(p2("880086C3E88112"), 7);
+    assert_eq!(p2("CE00C43D881120"), 9);
+    assert_eq!(p2("D8005AC2A8F0"), 1);
+    assert_eq!(p2("F600BC2D8F"), 0);
+    assert_eq!(p2("9C005AC2F8F0"), 0);
+    assert_eq!(p2("9C0141080250320F1802104A08"), 1);
+}
